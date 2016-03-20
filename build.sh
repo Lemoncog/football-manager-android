8b9a70fd59c99b9ee0f9e2c067b7d3bbc0c600d0
Lemoncog <lemoncog@gmail.com>
2016-03-20 00:12:20 +0000

    too tired, this shouldnt be here

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $CI_HOME/football-manager
-./gradlew testVersions -PversionCode=$TRAVIS_BUILD_NUMBER -PversionName=0.0.$TRAVIS_BUILD_NUMBER testVersions build
+./gradlew -PversionCode=$TRAVIS_BUILD_NUMBER -PversionName=0.0.$TRAVIS_BUILD_NUMBER testVersions build
