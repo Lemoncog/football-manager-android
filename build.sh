0fdcc16915ef462b730d00e3cf363ba1c9ef7925
Lemoncog <lemoncog@gmail.com>
2016-04-04 19:21:23 +0100

    Adding property injection to allow for building with keystore

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 echo 'versionCode:'$1
 echo 'versionName:'$2
+keystorePath=$3
+keystorePassword=$4
+keyAlias=$5
+keyPassword=$6
 cd football-manager
-./gradlew -PversionCode=$1 -PversionName=$2 testVersions build
+./gradlew -PversionCode=$1 -PversionName=$2 -PkeystorePath=$keystorePath -PkeystorePassword=$keystorePassword -PkeyAlias=$keyAlias -PkeyPassword=$keyPassword  testVersions build
